5ffc1feac2031a617c767be1b78885c63943299b
slpitz <slpitz@gmail.com>
2020-02-03 18:48:48 -0500

    added cropped dataframe from photo, getNeighbors function

--- bmoreRoofStream.py
+++ bmoreRoofStream.py
@@ -6,21 +6,82 @@ Created on Thu Jan 23 15:27:53 2020
 """
 
 import streamlit as st
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
 
 #to make things easier later, also import...
 import numpy as np
 import pandas as pd
 
+#other libraries
+import rasterio
+from shapely.geometry import box
+import geopandas as gpd
+from fiona.crs import from_epsg
+
+def getNeighbors(geoTiff, bigdf): #geotiff, dataframe to parse, new dataframeName
+    """Function to show image of vacant property"""
+    
+    loadedGeotif = rasterio.open(geoTiff)
+    #loadedGeotif.bounds
+    #bounding box, assumes box is north south
+    northlat = loadedGeotif.bounds[3]
+    southlat = loadedGeotif.bounds[1]
+    westLong = loadedGeotif.bounds[0]
+    eastLong = loadedGeotif.bounds[2]
+    
+    newdf = bigdf[(bigdf['lat'] > southlat) & 
+                            (bigdf['lat'] < northlat) &
+                            (bigdf['lon'] < eastLong)&
+                            (bigdf['lon'] > westLong)]
+    return newdf
+    #return df that has the addresses in the photo
+
 add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+    'Select an address:',
+    ('Payson', 'Fulton'))
+
+
+onlyfiles = [f for f in listdir("AddressPhotos/") if isfile(join("AddressPhotos/", f))]
+imageselect = st.sidebar.selectbox("Pick an image.", onlyfiles)
+
+#Contact Info
+st.sidebar.markdown("""
+### Contact 
+This website was created by Scott P (insert email).""")
 
 st.image('CITY-LOGO.png', caption="Charm City", width=100)
+#st.image('1900-PENROSE-AVE.tif', caption="Charm City", width=400)
+
 
 st.title('Project Skylight')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('Vacant_Buildings_lat_long.csv')
+
+#st.title('Animal Identification')
+st.write("Pick an image from the left. You'll be able to view the image.")
+st.write("When you're ready, submit a prediction on the left.")
+
+image = Image.open("AddressPhotos/" + imageselect)
+st.image(image, caption="Let's predict roof integrity.", width=200)
+
+loadedGeotif = rasterio.open("AddressPhotos/" + imageselect)
+#st.write(loadedGeotif.bounds)
+
+
+#load shapefile deried csv
+st.write("load real property file")
+realPropShape1 = pd.read_csv('CondensedRealPropShapeData.csv')
+
+#run function to produce
+st.write("show df of photo")
+closeby = getNeighbors("AddressPhotos/" + imageselect, realPropShape1)
+st.write(closeby)
+
+
+
 
 @st.cache
 def load_data(nrows):
@@ -33,7 +94,7 @@ def load_data(nrows):
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 1000 rows of data into the dataframe.
-data = load_data(4000)
+data = load_data(8000)
 # Notify the reader that the data was successfully loaded.
 st.write('Done! (using st.cache)')
 
