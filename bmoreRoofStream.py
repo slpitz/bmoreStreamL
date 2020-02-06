177220d1684ab5a8313346ea3fe46adfcb946619
slpitz <slpitz@gmail.com>
2020-02-06 18:46:29 -0500

    added probabilities from CNN

--- bmoreRoofStream.py
+++ bmoreRoofStream.py
@@ -44,37 +44,58 @@ def subsetVac(divisor):
     filtered = dataframe1.iloc[remain]
     return filtered 
 
+######################################################
+###################   Sidebar ########################
+######################################################
+
 st.sidebar.image('CITY-LOGO.png', caption="Charm City", width=100)
 
 testSetDataframe = pd.read_csv('testSetPreds.csv')
 
 dropDownAddress = testSetDataframe[['dashAddress']]
 
-st.write(dropDownAddress['dashAddress'].values.tolist())
+#st.write(dropDownAddress['dashAddress'].values.tolist())
 
 onlyfiles = [f for f in listdir("AddressPhotos/") if isfile(join("AddressPhotos/", f))]
 #st.write(type(onlyfiles))
-imageselect = st.sidebar.selectbox("Pick an image.", dropDownAddress['dashAddress'].values.tolist())
-st.write(onlyfiles)
+imageselect = st.sidebar.selectbox("Pick an address from the menu", dropDownAddress['dashAddress'].values.tolist())
+#st.write(onlyfiles)
 
 #Contact Info
 st.sidebar.markdown("""
 ### Contact 
 This website was created by Scott Pitz (insert email).""")
 
+######################################################
+###################  Main Page #######################
+######################################################
+
 st.title('Project Skylight')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('Vacant_Buildings_lat_long.csv')
 
-#st.title('Animal Identification')
-st.write("Pick an image from the left. You'll be able to view the image.")
+st.write("Pick an image from the drop down menu. You'll be able to view the image.")
 #st.write("When you're ready, submit a prediction on the left.")
 
-image = Image.open("AddressPhotos/" + imageselect)
+image = Image.open("AddressPhotos/" + imageselect+ ".tif")
 st.image(image, caption="Let's predict roof integrity.", width=300)
 
-loadedGeotif = rasterio.open("AddressPhotos/" + imageselect)
+######################################################
+st.write("Center Address:  "+ imageselect) 
+#st.write(list(testSetDataframe))
+#american = testSetDataframe[testSetDataframe['dashAddress'] == imageselect, 'Collapsed']
+#testSetDataframe.loc[testSetDataframe['dashAddress'] == imageselect, 'Collapsed']
+solidProb = testSetDataframe.loc[testSetDataframe['dashAddress'] == imageselect].iloc[0]['Solid']
+st.write("The Solid Probability is:  " + str(solidProb.round(3)))
+#df.loc[df['B'] == 3, 'A'].iloc[0]
+
+######################################################
+#Load neighbors data
+
+rasterFileName = str(imageselect+ ".tif")
+#st.write(rasterFileName)
+loadedGeotif = rasterio.open("AddressPhotos/" + rasterFileName)
 #st.write(loadedGeotif.bounds)
 
 
@@ -84,10 +105,13 @@ realPropShape1 = pd.read_csv('CondensedRealPropShapeData.csv')
 
 #run function to produce
 #st.write("show df of photo")
-closeby = getNeighbors("AddressPhotos/" + imageselect, realPropShape1)
+
+closeby = getNeighbors("AddressPhotos/" + imageselect+ ".tif", realPropShape1)
 #st.write(closeby)
 
 st.table(closeby)
+
+######################################################
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('Vacant_Buildings_lat_long.csv')
@@ -117,7 +141,7 @@ st.write('Done! (using st.cache)')
 
 st.subheader('Subset of current Vacant Homes')
 smallerVacantdf = subsetVac(15)
-
+s
 #st.write('Subset of vacant homes from across Baltimore')
 st.map(smallerVacantdf)
 st.write(smallerVacantdf)
